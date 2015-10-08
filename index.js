92e264f8eecb8ca9b71b58ebee948ecf06a88b20
Jake Hickenlooper <jake@weboftomorrow.com>
2015-10-08 09:57:04 -0600

    Allow multiple css files in args
    Drop the usage of -i for input files. Add newline between groups of css
    sources.

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 var argv = require("yargs")
-  .usage('Usage: $0 [--input|-i input.css]')
-  .alias('i', 'input')
-  .requiresArg(['i'])
+  .usage('Usage: $0 [input.css]')
   .help('h')
   .alias('h', 'help')
   .check(function(argv){
-    if (!argv.input) {
+    if (!argv._) {
       throw "Need to specify an input css file.";
     }
     return true;
@@ -16,15 +14,16 @@ var fs = require("fs")
 var postcss = require("postcss")
 var atImport = require("postcss-import")
 
+argv._.forEach(function(input){
 // css to be processed
-var css = fs.readFileSync(argv.input, "utf8")
+var css = fs.readFileSync(input, "utf8")
 
 // process css
 var output = postcss()
   .use(atImport())
   .process(css, {
     // `from` option is required so relative import can work from input dirname
-    from: argv.input,
+    from: input,
     map: {
       inline: false
     }
@@ -35,4 +34,6 @@ var output = postcss()
       sources = result.map.toJSON().sources;
     }
     process.stdout.write(sources.join(' '));
+    process.stdout.write('\n');
   });
+});
